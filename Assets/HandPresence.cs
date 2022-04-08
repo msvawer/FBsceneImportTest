bfe608e44678895b1aef1dd52ce546aa5dacbf80
MichVawer <michelle.vawer@icloud.com>
2022-04-08 14:31:46 -0400

    set up jukebox

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        foreach (var item in devices)
+            {
+            Debug.Log(item.name + item.characteristics);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
