e3a64544037e7436761aee172b9a4c7c19cdcc52
MichVawer <michelle.vawer@icloud.com>
2022-04-20 16:10:40 -0400

    radio alert waitforSeconds adjustments

---
+++ Assets/RadioAnnouncement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class RadioAnnouncement : MonoBehaviour
+{
+    public AudioSource radioSrc;
+    public AudioClip radioClip;
+    public AudioClip beepBoopClip;
+
+    void Start()
+    {
+        StartCoroutine(playSound());
+    }
+
+    IEnumerator playSound()
+    {
+        yield return new WaitForSeconds(150);
+        GetComponent<AudioSource>().clip = radioClip;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(radioClip.length);
+        GetComponent<AudioSource>().clip = beepBoopClip;
+        GetComponent<AudioSource>().Play();
+        
+    }
+}
